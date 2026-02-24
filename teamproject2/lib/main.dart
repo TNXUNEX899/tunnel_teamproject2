8f61fb6e2f664418a55cf592378ae6b649368b39
antthanapa <ant.thanapa@gmail.com>
2026-02-24 09:48:29 +0700

    edit

--- teamproject2/lib/main.dart
+++ teamproject2/lib/main.dart
@@ -1,59 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:teamproject2/screens/open_map.dart';
 import 'package:provider/provider.dart';
-import 'package:teamproject2/provider/current_location_provider.dart'; 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:teamproject2/screens/open_map.dart';
+import 'package:teamproject2/provider/current_location_provider.dart'; 
 import 'firebase_options.dart';
 
 void main() async {
+  // 1. Initialized ตะกร้าของ Flutter และ Firebase
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  
+  // 2. เอา Provider ครอบไว้บนสุดของแอป (ครอบ MyApp)
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => CurrentLocationProvider()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // 3. มี MaterialApp แค่ตัวเดียว
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flood Tunnel Alert',
+      debugShowCheckedModeBanner: false, // ปิดแถบ Debug มุมขวาบน
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
       ),
-      home: HomeScreen()
-    );
-  }
-}
-
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-// แก้ไขในส่วน _HomeScreenState
-class _HomeScreenState extends State<HomeScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => CurrentLocationProvider()),
-      ],
-      child: MaterialApp(
-        title: 'Flood Tunnel Alert',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          useMaterial3: true,
-        ),
-        home: const OpenMap(), 
-      ),
+      home: const OpenMap(), // กำหนดให้หน้า OpenMap เป็นหน้าแรกของแอป
     );
   }
 }
